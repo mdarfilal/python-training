2440e09aabc1b1600cbefed14be076bc234f1927
mdarficops <arabic91@hotmail.fr>
2016-10-27 18:57:00 +0200

    add type of sort, usage

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,19 +6,30 @@ print("bubblesort starting")
 #Name of script without path
 scriptName = os.path.basename(sys.argv[0]);
 
+Usage = """Usage : ,scriptName, Order tabToSort
+Usage : with Order egal asc OR desc
+Usage : with tabToSort like : 1,8,74,23"""
+
+#Parse values to sort
+tab2=list(map(int, sys.argv[2].split(",")))
+
 #Check number of parameters
-if(len(sys.argv) < 2):
-	print("Usage :" ,scriptName, "tabToSort")
-	print("Usage : with tabToSort like : 1,8,74,23")
+if(len(sys.argv) < 3):
+	print(Usage)
 	#sys.exit()
 	quit()
-	
-#Parse argument
-tab2=list(map(int, sys.argv[1].split(",")))
 
-print("Values to sort",tab2)
+def sortAscValues():
+	print("Values to sort asc",tab2)
+	tabSortedAsc = sortAsc(tab2)
+	print("Values sorted asc",tabSortedAsc)
 
-def sort(array):
+def sortDescValues():
+	print("Values to sort desc",tab2)
+	tabSortedDesc = sortDesc(tab2)
+	print("Values sorted desc",tabSortedDesc)
+
+def sortAsc(array):
 	a=0
 	while a<len(array)-1:
 		i=0
@@ -29,7 +40,29 @@ def sort(array):
 		a+=1
 	return array
 
-tabSorted = sort(tab2)
-		
-print("Values sorted",tabSorted)
+def sortDesc(array):
+	a=0
+	while a<len(array)-1:
+		i=0
+		while i<len(array)-1:
+			if (array[i] < array[i+1]):
+				array[i], array[i+1] = array[i+1], array[i]
+			i+=1
+		a+=1
+	return array
+
+######################################
+#				MAIN
+######################################
+
+#Check type of sort
+if (sys.argv[1]=="asc"):
+	sortAscValues()
+elif (sys.argv[1]=="desc"):
+	sortDescValues()
+else:
+	print ("No function found")
+	print(Usage)
+	quit()
+	
 print ("finish")
