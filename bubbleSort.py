ef42a6ec0de7dffd92354e38e2915abfcc80f158
mdarficops <arabic91@hotmail.fr>
2016-10-30 15:19:24 +0100

    Refactor
    delete white space end of lines
    add docstrings
    name of methods and attributes
    add white space around operator

--- bubbleSort.py
+++ bubbleSort.py
@@ -5,20 +5,26 @@ print("*******************")
 print(" Bubblesort starting")
 print("*******************")
 
-#Name of script without path
-scriptName = os.path.basename(sys.argv[0])
+######################################
+#				DECLARATIONS
+######################################
 
-usage = "Usage : " + scriptName + " Order tabToSort \n" \
+#Name of script without path
+script_name = os.path.basename(sys.argv[0])
+
+usage = "Usage : " + script_name + " Order tab_to_sort \n" \
 	"Usage : with Order egal asc OR desc \n" \
-	"Usage : with tabToSort like : 1,8,74,23 \n" \
-	"Test usage : " +scriptName+ " test all"
+	"Usage : with tab_to_sort like : 1,8,74,23 \n" \
+	"Test usage : " + script_name + " test all"
 
 #Variables
-nbTestKo=0
+nb_test_ko=0
+tab_to_sort=[]
 
 ORDER_ASC="asc"
 ORDER_DESC="desc"
-Test="test"
+TEST="test"
+ALL="all"
 
 #Check number of parameters, Three required
 if(len(sys.argv) != 3):
@@ -26,89 +32,115 @@ if(len(sys.argv) != 3):
 	print(usage)
 	sys.exit()
 
-#Parse values to sort
-def parseArgs():
+######################################
+#				FUNCTIONS
+######################################
+
+def parse_values_parameter():
+	"""Parse values received in the third parameter of script parameters."""
+	global tab_to_sort
 	try:
-		tab2=list(map(int, sys.argv[2].split(",")))
+		tab_to_sort = list(map(int, sys.argv[2].split(",")))
 	except ValueError:
 		print("Can't parse values : ", sys.argv[2])
 		print(usage)
 		sys.exit()
 
-def sortAscValues():
-	parseArgs()
-	print("Values to sort asc",tab2)
-	tabSortedAsc = sortAsc(tab2)
-	print("Values sorted asc",tabSortedAsc)
+def sort_asc_values():
+	"""Print received array in script parameter to sort asc and sort result."""
+	parse_values_parameter()
+	print("Values to sort asc",tab_to_sort)
+	tab_sorted_asc = sort_asc(tab_to_sort)
+	print("Values sorted asc",tab_sorted_asc)
 
-def sortDescValues():
-	parseArgs()
-	print("Values to sort desc",tab2)
-	tabSortedDesc = sortDesc(tab2)
-	print("Values sorted desc",tabSortedDesc)
+def sort_desc_values():
+	"""Print received array in script parameter to sort desc and sort result."""
+	parse_values_parameter()
+	print("Values to sort desc",tab_to_sort)
+	tab_sorted_desc = sort_desc(tab_to_sort)
+	print("Values sorted desc",tab_sorted_desc)
 
-def sortAsc(array):
-	a=0
-	while a<len(array)-1:
-		i=0
-		while i<len(array)-1:
-			if (array[i] > array[i+1]):
-				array[i], array[i+1] = array[i+1], array[i]
-			i+=1
-		a+=1
-	return array
-
-def sortDesc(array):
-	a=0
-	while a<len(array)-1:
-		i=0
-		while i<len(array)-1:
-			if (array[i] < array[i+1]):
-				array[i], array[i+1] = array[i+1], array[i]
-			i+=1
-		a+=1
-	return array
-
-def compareArray(array, type):
-	global nbTestKo
+def sort_asc(array):
+	"""Sort asc array given in param.
+	@param :  array to sort asc
+	@return : array sorted asc
 	
-	if (type == ORDER_ASC):
-		manualSort = sortAsc(array)
-		pythonSort = sorted(array)
+	"""
+	a = 0
+	while a < len(array)-1:
+		i = 0
+		while i < len(array)-1:
+			if(array[i] > array[i+1]):
+				array[i], array[i+1] = array[i+1], array[i]
+			i += 1
+		a += 1
+	return array
+
+def sort_desc(array):
+	"""Sort desc array given in param.
+	@param :  array to sort desc
+	@return : array sorted desc
+	
+	"""
+	a = 0
+	while a < len(array)-1:
+		i = 0
+		while i < len(array)-1:
+			if(array[i] < array[i+1]):
+				array[i], array[i+1] = array[i+1], array[i]
+			i += 1
+		a += 1
+	return array
+
+
+def compare_array(array, type):
+	"""Compare array given in param and the result with python sorted method.
+	If the arrays aren't equal, print error and increment test's variable.
+	@param :  array to sort
+	@param : type of sort (asc or desc)
+	
+	"""
+	global nb_test_ko
+
+	if(type == ORDER_ASC):
+		manual_sort = sort_asc(array)
+		python_sort = sorted(array)
 	else:
-		manualSort = sortDesc(array)
-		pythonSort = sorted(array, reverse=True)
+		manual_sort = sort_desc(array)
+		python_sort = sorted(array, reverse=True)
+
+	if(manual_sort != python_sort):
+		print("Error in sorting array, expected : ", python_sort, " but was ",manual_sort)
+		nb_test_ko += 1
+
+def all_sorts_test():
+	"""Sort Tests method.
+	Print if tests are OK or not.
 	
-	if (manualSort != pythonSort):
-		print("Error in sorting array, expected : ", pythonSort, " but was ",manualSort)
-		nbTestKo += 1
-		
-def allSortTest():
+	"""
 	print("Test all sorts")
-	compareArray([1,7,5], ORDER_ASC)
-	
-	compareArray([1,7,5], ORDER_DESC)
-	
-	if (nbTestKo==0):
+	compare_array([1,7,5], ORDER_ASC)
+	compare_array([1,7,5], ORDER_DESC)
+
+	if(nb_test_ko == 0):
 		print("All Tests OK")
 	else:
-		print(nbTestKo, "tests are KO")
+		print(nb_test_ko, "tests are KO")
 
 ######################################
 #				MAIN
 ######################################
 
 #Check type of sort
-if (sys.argv[1]==ORDER_ASC):
-	sortAscValues()
-elif (sys.argv[1]==ORDER_DESC):
-	sortDescValues()
-elif (sys.argv[1]==Test and sys.argv[2]=="all"):
-	allSortTest()
+if(sys.argv[1] == ORDER_ASC):
+	sort_asc_values()
+elif(sys.argv[1] == ORDER_DESC):
+	sort_desc_values()
+elif(sys.argv[1] == TEST and sys.argv[2] == ALL):
+	all_sorts_test()
 else:
-
 	print("No function found")
 	print(usage)
 	sys.exit()
-	
-print ("Finish")
+
+print("Finish")
