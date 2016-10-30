42320705bd7620eaf50fa904876b758735d1c19e
mdarficops <arabic91@hotmail.fr>
2016-10-30 12:32:07 +0100

    Add tests

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,36 +2,47 @@ import sys
 import os
 
 print("*******************")
-print("bubblesort starting")
+print(" Bubblesort starting")
 print("*******************")
 
 #Name of script without path
-scriptName = os.path.basename(sys.argv[0]);
+scriptName = os.path.basename(sys.argv[0])
 
-Usage = """Usage : ,scriptName, Order tabToSort
-Usage : with Order egal asc OR desc
-Usage : with tabToSort like : 1,8,74,23"""
+usage = "Usage : " + scriptName + " Order tabToSort \n" \
+	"Usage : with Order egal asc OR desc \n" \
+	"Usage : with tabToSort like : 1,8,74,23 \n" \
+	"Test usage : " +scriptName+ " test all"
+
+#Variables
+nbTestKo=0
+
+ORDER_ASC="asc"
+ORDER_DESC="desc"
+Test="test"
 
 #Check number of parameters, Three required
 if(len(sys.argv) != 3):
 	print("Three parameters required")
-	print(Usage)
+	print(usage)
 	sys.exit()
 
 #Parse values to sort
-try:
-	tab2=list(map(int, sys.argv[2].split(",")))
-except ValueError:
-	print("Can't parse values : ", sys.argv[2])
-	print(Usage)
-	sys.exit()
+def parseArgs():
+	try:
+		tab2=list(map(int, sys.argv[2].split(",")))
+	except ValueError:
+		print("Can't parse values : ", sys.argv[2])
+		print(usage)
+		sys.exit()
 
 def sortAscValues():
+	parseArgs()
 	print("Values to sort asc",tab2)
 	tabSortedAsc = sortAsc(tab2)
 	print("Values sorted asc",tabSortedAsc)
 
 def sortDescValues():
+	parseArgs()
 	print("Values to sort desc",tab2)
 	tabSortedDesc = sortDesc(tab2)
 	print("Values sorted desc",tabSortedDesc)
@@ -58,18 +69,45 @@ def sortDesc(array):
 		a+=1
 	return array
 
+def compareArray(array, type):
+	global nbTestKo
+	
+	if (type == ORDER_ASC):
+		manualSort = sortAsc(array)
+		pythonSort = sorted(array)
+	else:
+		manualSort = sortDesc(array)
+		pythonSort = sorted(array, reverse=True)
+	
+	if (manualSort != pythonSort):
+		print("Error in sorting array, expected : ", pythonSort, " but was ",manualSort)
+		nbTestKo += 1
+		
+def allSortTest():
+	print("Test all sorts")
+	compareArray([1,7,5], ORDER_ASC)
+	
+	compareArray([1,7,5], ORDER_DESC)
+	
+	if (nbTestKo==0):
+		print("All Tests OK")
+	else:
+		print(nbTestKo, "tests are KO")
+
 ######################################
 #				MAIN
 ######################################
 
 #Check type of sort
-if (sys.argv[1]=="asc"):
+if (sys.argv[1]==ORDER_ASC):
 	sortAscValues()
-elif (sys.argv[1]=="desc"):
+elif (sys.argv[1]==ORDER_DESC):
 	sortDescValues()
+elif (sys.argv[1]==Test and sys.argv[2]=="all"):
+	allSortTest()
 else:
 	print("No function found")
-	print(Usage)
+	print(usage)
 	sys.exit()
 	
-print ("finish")
+print ("Finish")
