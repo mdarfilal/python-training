70e3d3b6e86000f7b70596a4cca89d34018c00cc
mdarficops <arabic91@hotmail.fr>
2016-11-04 21:16:36 +0100

    Review random, renam white vote to blank vote

--- histoVote.py
+++ histoVote.py
@@ -16,9 +16,9 @@ if(len(sys.argv) != 4):
 
 vote_first_candidate = 0
 vote_second_candidate = 0
-white_vote = 0
+blank_vote = 0
 
-NAME_WHITE_VOTE = "White votes"
+NAME_BLANK_VOTE = "Blank votes"
 NAME_FIRST_CANDIDATE = sys.argv[1]
 NAME_SECOND_CANDIDATE = sys.argv[2]
 NUMBER_OF_CITIZENS = int(sys.argv[3])
@@ -28,19 +28,17 @@ def vote_of_citizens():
 	""" Simulate vote of citizen. """
 	global vote_first_candidate
 	global vote_second_candidate
-
+	global blank_vote
+	
 	for i in range(NUMBER_OF_CITIZENS):
 		vote = random.randint(1,10)
-		random.shuffle(CANDIDATES)
-		if(CANDIDATES[0] == NAME_FIRST_CANDIDATE and vote == 1):
-			vote_first_candidate+=1
-		elif(CANDIDATES[0] == NAME_SECOND_CANDIDATE and vote == 2):
-			vote_second_candidate+=1
 
-def calculate_white_vote():
-	""" Calculate number of white votes. """
-	global white_vote
-	white_vote = NUMBER_OF_CITIZENS - vote_first_candidate - vote_second_candidate
+		if(vote <= 3):
+			vote_first_candidate+=1
+		elif(vote > 3 and vote <= 6):
+			vote_second_candidate+=1
+		else:
+			blank_vote+=1
 	
 def show_result_on_stick_graph():
 	""" Show the result of vote on a stick graph. """
@@ -52,7 +50,7 @@ def show_result_on_stick_graph():
 	data = [vote_first_candidate, vote_second_candidate]
 	labels = [NAME_FIRST_CANDIDATE, NAME_SECOND_CANDIDATE]
 
-	plt.axis([0,2,0,NUMBER_OF_CITIZENS/4])
+	plt.axis([0,2,0,NUMBER_OF_CITIZENS/2])
 	plt.xlabel('Candidates')
 	plt.ylabel('Number of votes')
 	plt.xticks(horizontal_axis, labels)
@@ -65,8 +63,8 @@ def show_result_on_circular_digram():
 	print(NAME_FIRST_CANDIDATE, " " ,vote_first_candidate)
 	print(NAME_SECOND_CANDIDATE, " " ,vote_second_candidate)
 
-	labels = [NAME_FIRST_CANDIDATE, NAME_SECOND_CANDIDATE, NAME_WHITE_VOTE]
-	data = [vote_first_candidate, vote_second_candidate, white_vote]
+	labels = [NAME_FIRST_CANDIDATE, NAME_SECOND_CANDIDATE, NAME_BLANK_VOTE]
+	data = [vote_first_candidate, vote_second_candidate, blank_vote]
 
 	plt.pie(data, labels=labels, autopct='%1.1f%%', startangle=90, shadow=True)
 	plt.axis('equal')
@@ -74,6 +72,5 @@ def show_result_on_circular_digram():
 
 print("Who will be the next president")
 vote_of_citizens()
-calculate_white_vote()
 #show_result_on_stick_graph()
 show_result_on_circular_digram()
